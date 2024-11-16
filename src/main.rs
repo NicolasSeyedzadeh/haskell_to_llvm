687786cddf4bad24d26a35b489c8a94cddddf8db
NicolasSeyedzadeh <ns896@cam.ac.uk>
2024-11-16 13:35:28 +0000

    parse basic string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tree_sitter::{InputEdit, Language, Parser, Point};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    parser.set_language(&tree_sitter_haskell::LANGUAGE.into()).expect("Error loading Haskell grammar");
+    let source_code = "sumtorial :: Integer -> Integer\nsumtorial 0 = 0\nsumtorial n = n + sumtorial (n - 1)";
+    let tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
+
+    let ast_as_str=root_node.to_string();
+    println!("{}",ast_as_str);
+
 }
+
