dfefc6820651a6c3b08027cb2a882e293e3653ea
NicolasSeyedzadeh <ns896@cam.ac.uk>
2024-11-16 13:55:28 +0000

    More Linting

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use tree_sitter::Parser;
 
 fn main() {
     let mut parser = Parser::new();
-    parser.set_language(&tree_sitter_haskell::LANGUAGE.into()).expect("Error loading Haskell grammar");
-    let source_code = "sumtorial :: Integer -> Integer\nsumtorial 0 = 0\nsumtorial n = n + sumtorial (n - 1)";
+    parser
+        .set_language(&tree_sitter_haskell::LANGUAGE.into())
+        .expect("Error loading Haskell grammar");
+    let source_code =
+        "sumtorial :: Integer -> Integer\nsumtorial 0 = 0\nsumtorial n = n + sumtorial (n - 1)";
     let tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
 
-    let ast_as_str=root_node.to_string();
-    println!("{}",ast_as_str);
-
+    let ast_as_str = root_node.to_string();
+    println!("{}", ast_as_str);
 }
