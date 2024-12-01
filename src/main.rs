37969ec519eb50836f525666030ff813d56e15d8
NicolasSeyedzadeh <ns896@cam.ac.uk>
2024-12-01 19:05:40 +0000

    start ast matching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use inkwell::context::Context;
-use inkwell::targets::{Target, TargetMachine, TargetTriple};
+use inkwell::targets::{InitializationConfig, Target, TargetMachine, TargetTriple};
 use inkwell::OptimizationLevel;
 use tree_sitter::{Parser, Tree};
 fn main() {
-    let source_code = "3+5";
+    Target::initialize_native(&InitializationConfig::default())
+        .expect("Failed to initialize native target");
+    let source_code = "let x = 200 in x";
 
     //TODO: make this come from the CLI
     let triple: &str = "";
-    let ast = parse_to_ast(source_code).root_node();
+    let ast = parse_to_ast(source_code);
+
+    //haskell declarations -> declarations -> top_splice -> actual ast
+    let code_ast = ast
+        .root_node()
+        .child(0)
+        .unwrap()
+        .child(0)
+        .unwrap()
+        .child(0)
+        .unwrap();
 
     let context = Context::create();
     let module = context.create_module("my_module");
@@ -17,9 +29,7 @@ fn main() {
     } else {
         TargetTriple::create(triple)
     };
-    module.set_triple(&target_triple);
-
-    let target = Target::from_triple(&target_triple).unwrap();
+    let target = Target::from_triple(&target_triple).expect("Failed to get  from triple");
     let target_machine = target
         .create_target_machine(
             &target_triple,
@@ -36,13 +46,22 @@ fn main() {
     let i32_type = context.i32_type();
     let fn_type = i32_type.fn_type(&[], false);
     let function = module.add_function("main", fn_type, None);
+
     let basic_block = context.append_basic_block(function, "entry");
     let return_value = i32_type.const_int(0, false);
 
     let builder = context.create_builder();
     builder.position_at_end(basic_block);
+    //allocate an int onto the stack
+    let stack_loc = builder.build_alloca(i32_type, "name").expect("msg");
+
+    // store the value in location
+    let _ = builder.build_store(stack_loc, i32_type.const_int(3, false));
+
+    println!("{}", recursive_build(code_ast, source_code, &builder));
+
     let _ = builder.build_return(Some(&return_value));
-    module.print_to_stderr();
+    //module.print_to_stderr();
 }
 
 fn parse_to_ast(source_code: &str) -> Tree {
@@ -52,4 +71,20 @@ fn parse_to_ast(source_code: &str) -> Tree {
         .expect("Error loading Haskell grammar");
 
     parser.parse(source_code, None).unwrap()
+}
+
+fn get_val_from_node(ast: tree_sitter::Node, code: &str) -> String {
+    code[ast.byte_range()].to_string()
+}
+
+fn recursive_build(
+    ast: tree_sitter::Node,
+    code: &str,
+    _builder: &inkwell::builder::Builder,
+) -> String {
+    //match top level with string
+    match ast.grammar_name() {
+        "let_in" => get_val_from_node(ast.child(1).unwrap(), code), //ast. child 0 = "let", child 1 = match expression, child 2 = "in", child 3 = expression
+        _ => "Did not find a match".to_string(),
+    }
 }
